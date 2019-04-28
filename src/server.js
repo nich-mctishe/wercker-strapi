26ff81a91a6cf32576547ed40ebeaf939fed94cb
Nich Headlong <nichheadlong@googlemail.com>
2019-04-28 01:47:10 -0700

    new env vars

--- src/server.js
+++ src/server.js
@@ -10,11 +10,6 @@
 
 process.chdir(__dirname);
 
-console.log('DOKKU_MONGO_API_PORT_27017_TCP_ADDR', process.env.DOKKU_MONGO_API_PORT_27017_TCP_ADDR);
-console.log('DOKKU_MONGO_API_PORT_27017_TCP_PORT', process.env.DOKKU_MONGO_API_PORT_27017_TCP_PORT);
-console.log('DB_USERNAME', process.env.DB_USERNAME);
-console.log('DB_PASSWORD', process.env.DB_PASSWORD);
-
 (() => {
   const strapi = require('strapi');
   strapi.start();
